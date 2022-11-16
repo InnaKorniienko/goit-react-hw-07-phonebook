30f5084d98340c963831d8720c67efdc51170c76
Inna <inna.kornieko@gmail.com>
2022-11-16 20:13:37 +0200

    Upd file

--- src/components/shared/api.js
+++ src/components/shared/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const instanceContacts = axios.create({
     baseURL: "https://636b89f57f47ef51e133461c.mockapi.io/contacts/contacts",
-    params: {
-        _limit: 12,
-    }
 });
 
 export const getContacts = async() => {
@@ -12,12 +9,12 @@ export const getContacts = async() => {
     return data;
 }
 
-export const addContact = async(data) => {
+export const addContact = async data => {
     const {data: result} = await instanceContacts.post("/", data);
     return result;
 }
 
-export const removeContact = async(id) => {
-    const {data} = await instanceContacts.delete(`/${id}`);
+export const removeContact = async id => {
+    const { data } = await instanceContacts.delete(`/${id}`);
     return data;
 }
--- src/redux/contacts/contacts-operation.js
+++ src/redux/contacts/contacts-operation.js
@@ -1,18 +1,6 @@
-// import actions from "./contacts-action";
 import * as api from "../../components/shared/api";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-// import Notiflix from "notiflix";
 
-
-// const isDublicate = ({ name, phone }, contacts) => {
-//     const normalizedName = name.toLowerCase();
-//     const normalizedNumber = phone.toLowerCase();
-//     const result = contacts.find(item => {
-//         return (normalizedName === item.name.toLowerCase() || normalizedNumber === item.phone.toLowerCase())
-//     }
-// );
-//     return result;
-// }
 
 export const fetchContacts = createAsyncThunk(
     "contacts/fetch",
@@ -26,68 +14,17 @@ export const fetchContacts = createAsyncThunk(
     }
     )
 
-// export const fetchContacts = () => {
-//     const func = async (dispatch) => {
-//         dispatch(actions.fetchContactsLoading());
-//         try {
-//             const data = await api.getContacts();
-//             dispatch(actions.fetchContactsSucess(data));
-//         } catch(error) {
-//             const { message, response } = error;
-//             const errorDate = {
-//                 message,
-//                 status: response.status,
-//             }
-//             dispatch(actions.fetchContactsError(errorDate));
-//         }
-//     }
-
-//     return func;
-// }
-
 export const addContact = createAsyncThunk(
     "contact/add",
-    async(data, {rejectWithValue}) => {
-    try {
-        const result = await api.addContact(data);
-        return result;
-    } catch (error) {
-        return rejectWithValue(error);
+    async({ name, phone }, {rejectWithValue}) => {
+        try {
+            const result = await api.addContact({name, phone});
+            return result;
+        } catch (error) {
+            return rejectWithValue(error);
+        }
     }
-},
-// {
-//     condition: (data, { getState }) => {
-//         const { contacts } = getState();
-//         if(isDublicate(data, contacts.items)) {
-//             return Notiflix.Notify.warning(`${data.name} or ${data.phone} is already in contact`)
-//         }
-//         return;
-//     }
-// }
-)
-
-// export const addContact = (data) => {
-//     const func = async (dispatch, getState) => {
-//         const { contacts }  = getState();
-//         if(isDublicate(data, contacts.items)) {
-//             return Notiflix.Notify.warning(`${contacts.name} or ${contacts.number} is already in contact`);
-//         }  
-//         try {
-//             dispatch(actions.addContactLoading());
-//             const result = await api.addContact(data);
-//             dispatch(actions.addContactSucess(result));
-//         } catch(error) {
-//             const { message, response } = error;
-//             const errorDate = {
-//                 message,
-//                 status: response.status,
-//             }
-//             dispatch(actions.addContactError(errorDate));
-//         }
-//     }
-//     return func;
-// }
-
+);
 
 export const removeContact = createAsyncThunk(
     "contact/remove",
@@ -100,21 +37,3 @@ export const removeContact = createAsyncThunk(
         }
     }
 );
-
-// export const removeContact = (id) => {
-//     const func = async (dispatch) => {
-//         try {
-//             dispatch(actions.removeContactLoading());
-//             await api.removeContact(id);
-//             dispatch(actions.removeContactSucess(id));
-//         } catch(error) {
-//             const { message, response } = error;
-//             const errorDate = {
-//                 message,
-//                 status: response.status,
-//             }
-//             dispatch(actions.removeContactError(errorDate));
-//         }
-//     }
-//     return func;
-// }
